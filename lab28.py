e89dc0da0db9ba6b692e3c5227fbfebd85c84324
MSoriano <mattisorojas0@gmail.com>
2026-08-28 15:52:16 -0400

    listo

--- lab28.py
+++ lab28.py
@@ -1,13 +1,21 @@
-import math
-def sum_dos(lista):
+import matplotlib.pyplot as plt
+import time
+def sum_dos(numeros):
     i = 0
-    while i < len(lista):
+    while i < len(numeros):
         k = i + 1
-        while k < len(lista):
-            suma = lista[i] + lista[k]
+        while k < len(numeros):
+            suma = numeros[i] + numeros[k]
             if suma == 0:
-                print(lista[i], "y", lista[k], "suman 0")
+                print(numeros[i], "y", numeros[k], "suman 0")
+            
             k += 1
         i += 1
     return 0
-sum_dos([-1, 1, 3, -1, -2, -3, 4, 5, 6, -4, -5, -6])
+tamaño = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+numeros = []
+
+
+sum_dos(numeros)
+plt.bar(range(len(numeros)), numeros)
+plt.show()
